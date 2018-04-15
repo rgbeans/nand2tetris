ce2b57e165c17fbf302aef7b0d65a64fed74648f
Dean Pavlovsky <dean.pavlovsky@gmail.com>
2018-04-14 21:42:43 -0400

    Vm Translator Complete, Run EXE file in command prompt

--- VMTranslator/VMTranslator/VMTranslator.cs
+++ VMTranslator/VMTranslator/VMTranslator.cs
@@ -9,31 +9,76 @@ namespace VMTranslator
         {
             if (args == null || args.Length < 1)
             {
-                var msg = "Expected one argument. The path to .vm file";
+                var msg = "Expected one argument. The path to a directory with *.vm files";
                 throw new ArgumentException(msg);
             }
 
-            var path = args[0];
-            if (!File.Exists(path))
+            var root = args[0];
+            if (!Directory.Exists(root))
             {
-                var msg = $"file {path} does not exist";
+                var msg = $"directory {root} does not exist";
                 throw new FileNotFoundException(msg);
             }
-            var outFile = Path.ChangeExtension(path, ".asm");
-            //using (var ctx = new Context(path, File.CreateText(outFile)))
-            //{
-            //    var callbacks = new ParserCallBacks
-            //    {
-            //        OnPop =
-            //            (segment, index) =>
-            //                AssemblyWriter.EmitPop(ctx, segment,
-            //                    index), 
-            //        OnPush = (segment, index) => AssemblyWriter.EmitPush(ctx, segment, index),
-            //        OnArithmeticCommand = command => AssemblyWriter.EmitOperator(ctx.Writer, command)
-            //    };
-            //    Parser.Parse(File.ReadAllLines(path), callbacks);
-            //    ctx.Writer.Flush();
-            //}
+
+            var paths = Directory.EnumerateFiles(root, "*.vm", SearchOption.AllDirectories);
+            var dirName = Path.GetFileName(root);
+            var outFile = Path.Combine(root, $"{dirName}.asm");
+            using (var ctx = new Context(File.CreateText(outFile)))
+            {
+                ctx.Writer.WriteLine("@256");
+                ctx.Writer.WriteLine("D=A");
+                ctx.Writer.WriteLine("@SP");
+                ctx.Writer.WriteLine("M=D");
+                var sysInit = Path.Combine(root, "Sys.vm");
+                if (File.Exists(sysInit))
+                {
+                    string EntryPoint = "Sys.Init".ToUpper();
+                    ctx.CurrentFunction = new FunctionContext(EntryPoint, 0);
+                    ctx.InputFilePath = "Sys.vm";
+                    AssemblyWriter.EmitCall(ctx, EntryPoint, 0);
+                }
+                foreach (var path in paths)
+                {
+                    Console.WriteLine($"processing {path}");
+                    var inputFile = Path.GetFileName(path);
+                    ctx.InputFilePath = inputFile;
+
+
+                    var callbacks = new ParserCallBacks
+                    {
+                        OnPop =
+                            (segment, index) =>
+                                AssemblyWriter.EmitPop(ctx, segment, index),
+                        OnPush = (segment, index) =>
+                            AssemblyWriter.EmitPush(ctx, segment, index),
+                        OnArithmeticCommand = command =>
+                            AssemblyWriter.EmitOperator(ctx.Writer, command),
+                        OnGoTo = label =>
+                            AssemblyWriter.EmitGoTo(ctx, label),
+                        OnIfGoTo = label =>
+                            AssemblyWriter.EmitIfGoTo(ctx, label),
+                        OnLabelDeclaration = label =>
+                            AssemblyWriter.EmitLabel(ctx, label),
+                        OnCall = (command, n) =>
+                            AssemblyWriter.EmitCall(ctx, command.ToUpper(), n),
+                        OnFuncDef = (functionName, n) =>
+                        {
+                            var name = functionName.ToUpper();
+                            ctx.CurrentFunction = new FunctionContext(name, n);
+                            AssemblyWriter.EmitFunction(ctx, name, n);
+                        },
+                        OnReturn = () =>
+                        {
+                            AssemblyWriter.EmitReturn(ctx);
+                        },
+
+                    };
+                    Parser.Parse(File.ReadAllLines(path), callbacks);
+                    ctx.Writer.Flush();
+
+                }
+            }
+
         }
     }
 }
